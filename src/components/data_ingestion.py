ae8117e5e8b1035ece84e17977fad8c477a552dd
Nandan <nandankhadpe1@gmail.com>
2025-03-18 01:59:25 +0530

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,9 +5,12 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTansformationConfig
 
 @dataclass
 class DataIngestionConfig:
+    #below does this artifacts/train.csv
     train_data_path: str =os.path.join('artifacts',"train.csv")
     test_data_path: str =os.path.join('artifacts',"test.csv")
     raw_data_path: str =os.path.join('artifacts',"data.csv")
@@ -15,6 +18,7 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
+        #self.ingestion_config.train_data_path will contain the path
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
@@ -24,16 +28,20 @@ class DataIngestion:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info("Read the dataset as dataframe")
             
+            #now creating new directory named artifacts artifacts folder is created
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
+            #pushing the file using pre made path artifacts/raw.csv
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split intiated")
-            
+
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
+            #pushing the file using pre made path artifacts/train.csv
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
+            #pushing the file using pre made path artifacts/test.csv
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
@@ -44,9 +52,11 @@ class DataIngestion:
                 
             )
         except Exception as e:
-            raise CustomException(e.sys)
+            raise CustomException(e,sys)
 
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
